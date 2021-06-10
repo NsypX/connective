fa248c4daa86e171b406901f276452ad8e882622
putskan <ben.lahav101@gmail.com>
2021-06-10 13:34:28 +0000

    testing webpack django

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,9 +1,23 @@
+const path = require("path")
+const webpack = require("webpack")
+const BundleTracker = require("webpack-bundle-tracker")
+
 module.exports = {
+  configureWebpack: {
+    context: __dirname,
+    entry: "./assets/js/index",
+    output: {
+    //   // path: path.resolve("../server/server/assets/webpack_bundles/"),
+      filename: "[name]-[hash].js",
+    },
+    outputDir: path.resolve("../server/server/assets/webpack_bundles/"),
+
+    plugins: [new BundleTracker({ filename: "./webpack-stats.json" })],
+  },
+
   devServer: {
     disableHostCheck: true,
   },
-
-  transpileDependencies: ["vuetify"],
 
   pluginOptions: {
     i18n: {
@@ -13,4 +27,18 @@ module.exports = {
       enableInSFC: false,
     },
   },
+
+  // // whitenoise related
+  // transpileDependencies: ["vuetify"],
+  // publicPath: "/static/spa/", // Should be STATIC_URL + path/to/build
+  // // outputDir: path.resolve(
+  // //   __dirname,
+
+  // // ), // Output to a directory in STATICFILES_DIRS
+  // outputDir: "/workspace/connective/server/spa_backend/spa",
+  // filenameHashing: false, // Django will hash file names, not webpack
+  // runtimeCompiler: true, // See: https://vuejs.org/v2/guide/installation.html#Runtime-Compiler-vs-Runtime-only
+  // devServer: {
+  //   writeToDisk: true, // Write files to disk in dev mode, so Django can serve the assets
+  // },
 }
